1095d62f96b2662d7046f60e22a8a773306337e6
Carlo456 <gusvalenzuela77@gmail.com>
2025-01-09 15:18:52 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">
+        Hello world!
+      </h1>
+      <button className="btn">Hello daisyUI</button>
+    </>
+  )
+}
+
+export default App
